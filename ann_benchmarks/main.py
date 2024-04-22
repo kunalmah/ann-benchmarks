f4ff67d8f2c4df94c48d73a0d314a3db38c2b227
Dong, Xin1 <xin1.dong@intel.com>
2024-04-22 18:48:14 +0800

    rool back

--- ann_benchmarks/main.py
+++ ann_benchmarks/main.py
@@ -72,7 +72,6 @@ def run_worker(cpu: int, args: argparse.Namespace, queue: multiprocessing.Queue)
             memory_margin = 500e6  # reserve some extra memory for misc stuff
             mem_limit = int((psutil.virtual_memory().available - memory_margin) / args.parallelism)
             cpu_limit = str(cpu) if not args.batch else f"0-{multiprocessing.cpu_count() - 1}"
-            logger.info(f"DBEUG:: mem_limit={mem_limit}, cpu_limit={cpu_limit}")
             
             run_docker(definition, args.dataset, args.count, args.runs, args.timeout, args.batch, cpu_limit, mem_limit)
 
